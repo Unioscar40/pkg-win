2420b06d257f179c2fbfc0f8a8559e94840f809e
Zettai <raulbeltmarc@gmail.com>
2024-02-03 10:44:21 +0100

    replace {name} for projectName

--- src/Builder.cpp
+++ src/Builder.cpp
@@ -1,9 +1,38 @@
 #include <Builder.hpp>
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 void Builder::CreateCmakeFile() {
-    std::filesystem::copy(std::filesystem::current_path()/"files/genericCmakeFile.txt",path/"CMakeLists.txt");
+    std::string projectName(this->projectName);
+    std::regex pattern("\\{name\\}");
+    std::filesystem::path filePath = std::filesystem::current_path()/"files/genericCmakeFile.txt";
+    std::filesystem::path tempFilePath = std::filesystem::temp_directory_path() / "temp_file.txt";
+
+    std::ifstream inputFile(filePath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return;
+    }
+
+    std::string content((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+    inputFile.close();
+
+    std::ofstream outputFile(tempFilePath);
+    if (!outputFile.is_open()) {
+        std::cerr << "Unable to open file: " << filePath << std::endl;
+        return;
+    }
+    outputFile << std::regex_replace(content, pattern, projectName);
+    outputFile.close();
+
+    std::filesystem::copy(tempFilePath, path/"CMakeLists.txt");
+
+    if (std::filesystem::remove(tempFilePath)) {
+        std::cout << "Temporary file " << tempFilePath << " has been removed." << std::endl;
+    } else {
+        std::cerr << "Failed to remove temporary file " << tempFilePath << "." << std::endl;
+    }
 }
 
 void Builder::CreateDirectoryStructure() {
